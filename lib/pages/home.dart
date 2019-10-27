0cf949bc951ffc9a5d08c98e11640400dca5792f
Waga <wagaodongo@gmail.com>
2019-10-27 16:23:31 +0300

    Initial

---
+++ lib/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Home");
+  }
+}
