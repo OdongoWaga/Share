529b371d3a1698bad5f8d644acae9d5d41f2734d
Waga <wagaodongo@gmail.com>
2019-10-27 20:30:57 +0300

    reusable Appbar widget

---
+++ lib/widgets/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+AppBar header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? "Share" : titleText,
+      style: TextStyle(
+          color: Colors.white,
+          fontFamily: isAppTitle ? "Signatra" : "",
+          fontSize: isAppTitle ? 50.0 : 22),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
+}
