0acbff70438cbd1a836efadff2ced830d126e155
Waga <wagaodongo@gmail.com>
2019-10-27 20:44:02 +0300

    Circular and Linear Progress Widgets

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared/widgets/header.dart';
+import 'package:shared/widgets/progress.dart';
 
 class Timeline extends StatefulWidget {
   @override
@@ -11,7 +12,7 @@ class _TimelineState extends State<Timeline> {
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text("Timeline"),
+      body: linearProgress(),
     );
   }
 }
