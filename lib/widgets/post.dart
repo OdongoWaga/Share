64e4de25b0a4afcd6d22fa6dd7e0accc422c6a79
Waga <wagaodongo@gmail.com>
2019-11-05 10:33:12 +0300

    Post Model

---
+++ lib/widgets/post.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Post extends StatefulWidget {
+  final String postId;
+  final String ownerId;
+  final String username;
+  final String location;
+  final String description;
+  final String mediaUrl;
+  final dynamic likes;
+
+  Post({
+    this.postId,
+    this.ownerId,
+    this.username,
+    this.location,
+    this.description,
+    this.mediaUrl,
+    this.likes,
+  });
+
+  factory Post.fromDocument(DocumentSnapshot doc) {
+    return Post(
+      postId: doc['postId'],
+      ownerId: doc['ownerId'],
+      username: doc['username'],
+      location: doc['location'],
+      description: doc['description'],
+      mediaUrl: doc['mediaUrl'],
+      likes: doc['likes'],
+    );
+  }
+
+  int getLikeCounter(likes) {
+    // if there are no likes return 0
+
+    if (likes == null) {
+      return 0;
+    }
+
+    int counter = 0;
+    //If key is set to true add a like
+    likes.values.forEach((val) {
+      if (val == true) {
+        counter += 1;
+      }
+    });
+    return counter;
+  }
+
+  @override
+  _PostState createState() => _PostState(
+        postId: this.postId,
+        ownerId: this.ownerId,
+        username: this.username,
+        location: this.location,
+        description: this.description,
+        mediaUrl: this.mediaUrl,
+        likes: this.likes,
+        likeCount: getLikeCounter(this.likes),
+      );
+}
+
+class _PostState extends State<Post> {
+  final String postId;
+  final String ownerId;
+  final String username;
+  final String location;
+  final String description;
+  final String mediaUrl;
+  int likeCount;
+  Map likes;
+
+  _PostState({
+    this.postId,
+    this.ownerId,
+    this.username,
+    this.location,
+    this.description,
+    this.mediaUrl,
+    this.likes,
+    this.likeCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
